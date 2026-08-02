ce0725d69b03b65df2c8dd2dcb71ea67848d8733
ErickMichellSalasLara <st4382@utr.edu.mx>
2026-08-02 16:42:59 -0600

    Preparando backend para despliegue en Railway

--- services/reservas.py
+++ services/reservas.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, HTTPException
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import httpx
+import os
+import json
 
 router = APIRouter()
 
@@ -23,12 +25,24 @@ async def obtener_dias_festivos(anio: int = 2026):
 @router.get("/eventos")
 def obtener_reservas():
     try:
-        creds = service_account.Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
+        # LÓGICA DE CREDENCIALES (NUBE VS LOCAL)
+        google_creds_env = os.getenv("GOOGLE_CREDENTIALS")
+
+        if google_creds_env:
+            # Si estamos en Railway, leemos la variable de entorno
+            creds_dict = json.loads(google_creds_env)
+            creds = service_account.Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
+        else:
+            # Si estamos en tu computadora local, leemos el archivo
+            creds = service_account.Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
+
         servicio = build('calendar', 'v3', credentials=creds)
         eventos_resultado = servicio.events().list(
             calendarId=CALENDAR_ID, maxResults=10, singleEvents=True, orderBy='startTime'
         ).execute()
+
         eventos = eventos_resultado.get('items', [])
         return {"mensaje": "Conexión exitosa", "eventos": eventos}
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error conectando con Google Calendar: {str(e)}")
